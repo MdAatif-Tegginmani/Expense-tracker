7b6e08397731ea6b12687170fcea9ea4e6d85fe7
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2022-12-17 03:17:25 +0530

    expnse

---
+++ index.js
@@ -0,0 +1,51 @@
+function savetoLocalstorage(event) {
+    event.preventDefault();
+    
+let userDetails = {
+    My_Expense_Amount : document.getElementById('amount').value,
+    Description: document.getElementById('des').value,
+    category :document.getElementById('cat').value
+  
+}
+
+let userDetails_serialized=JSON.stringify(userDetails)  
+
+localStorage.setItem(userDetails.My_Expense_Amount , userDetails_serialized)
+showNewUseronScreen(userDetails)
+
+document.getElementById('amount').value ="" 
+document.getElementById('des').value ="" 
+document.getElementById('cat').value ="" 
+
+
+
+
+}
+
+function showNewUseronScreen(userDetails){
+    const d=document.getElementById('ul')
+    const li= `<li id="${userDetails.My_Expense_Amount}"> '${userDetails.My_Expense_Amount}','${userDetails.Description}','${userDetails.category}'
+     <button onclick = editUser('${ userDetails.My_Expense_Amount}','${userDetails.Description}','${userDetails.category}')> Edit </button> 
+     <button onclick = deleteUser('${userDetails.My_Expense_Amount}')> Delete </button> 
+      </li>`
+d.innerHTML=d.innerHTML + li
+   }
+
+ 
+
+   function deleteUser(amount) {
+       let child = document.getElementById(amount)
+       let parent=document.getElementById('ul')
+       parent.removeChild(child)
+       localStorage.removeItem(amount)
+   }
+
+   function editUser(amount,description,category) {
+       
+   document.getElementById('amount').value =amount ;
+   document.getElementById('des').value =description ;
+   document.getElementById('cat').value = category ;
+
+   deleteUser(amount)
+   
+   }
