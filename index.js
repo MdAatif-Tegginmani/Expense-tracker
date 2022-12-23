fd361f4bb00b2c0db98d40731949cf54baed3272
Md Aatif N Tegginmani <mdaatif84@gmail.com>
2022-12-24 00:26:59 +0530

    tracker

--- index.js
+++ index.js
@@ -1,51 +1,94 @@
-function savetoLocalstorage(event) {
+
+const url = "https://crudcrud.com/api/181bc287040a474aa759af7053ee37b7/data"
+
+
+async function savetoLocalstorage(event) {
     event.preventDefault();
-    
-let userDetails = {
-    My_Expense_Amount : document.getElementById('amount').value,
-    Description: document.getElementById('des').value,
-    category :document.getElementById('cat').value
-  
+
+    let userDetails = {
+        My_Expense_Amount: document.getElementById('amount').value,
+        Description: document.getElementById('des').value,
+        category: document.getElementById('cat').value
+
+    }
+
+    let userDetails_serialized = JSON.stringify(userDetails)
+
+    // localStorage.setItem(userDetails.My_Expense_Amount , userDetails_serialized)
+    const data = await axios.post(url, userDetails)
+    console.log(data);
+
+    document.getElementById('amount').value = ""
+    document.getElementById('des').value = ""
+    document.getElementById('cat').value = ""
+
+
+
+
 }
 
-let userDetails_serialized=JSON.stringify(userDetails)  
 
-localStorage.setItem(userDetails.My_Expense_Amount , userDetails_serialized)
-showNewUseronScreen(userDetails)
-
-document.getElementById('amount').value ="" 
-document.getElementById('des').value ="" 
-document.getElementById('cat').value ="" 
-
-
-
+function deleteUser(amount) {
+    let child = document.getElementById(amount)
+    let parent = document.getElementById('ul')
+    parent.removeChild(child)
 
 }
 
-function showNewUseronScreen(userDetails){
-    const d=document.getElementById('ul')
-    const li= `<li id="${userDetails.My_Expense_Amount}"> '${userDetails.My_Expense_Amount}','${userDetails.Description}','${userDetails.category}'
-     <button onclick = editUser('${ userDetails.My_Expense_Amount}','${userDetails.Description}','${userDetails.category}')> Edit </button> 
-     <button onclick = deleteUser('${userDetails.My_Expense_Amount}')> Delete </button> 
-      </li>`
-d.innerHTML=d.innerHTML + li
-   }
 
- 
 
-   function deleteUser(amount) {
-       let child = document.getElementById(amount)
-       let parent=document.getElementById('ul')
-       parent.removeChild(child)
-       localStorage.removeItem(amount)
-   }
 
-   function editUser(amount,description,category) {
-       
-   document.getElementById('amount').value =amount ;
-   document.getElementById('des').value =description ;
-   document.getElementById('cat').value = category ;
-
-   deleteUser(amount)
+async function deleteUser(Id) {
+    try {
+        const deleteNow = await axios.delete(`${url}/${Id}`)
+    }
+    catch(error){
+        console.log(error)
+    }
+ }
    
-   }
+
+
+
+
+
+
+
+function editUser( amount, description, category,Id) {
+
+    document.getElementById('amount').value = amount;
+    document.getElementById('des').value = description;
+    document.getElementById('cat').value = category;
+
+    deleteUser(Id) ;
+
+}
+
+
+
+
+window.addEventListener(`DOMContentLoaded`, async () => {
+    try {
+        const response = await axios.get(url)
+        console.log(response.data)
+        const d = document.getElementById(`ul`);
+        let li = ""
+
+        for (let i = 0; i < response.data.length; i++) {
+            li += `<li id="${response.data[i].My_Expense_Amount}"> '${response.data[i].My_Expense_Amount}','${response.data[i].Description}','${response.data[i].category}'
+            <button onclick = "editUser('${response.data[i].My_Expense_Amount}','${response.data[i].Description}',
+            '${response.data[i].category}' , '${response.data[i]._id}')"> Edit </button> 
+            <button onclick = deleteUser('${response.data[i]._id}')> Delete </button> 
+             </li>`
+        
+
+        }
+        d.innerHTML = d.innerHTML + li
+
+    }
+    catch (error) {
+        console.log(error);
+    }
+
+
+})
